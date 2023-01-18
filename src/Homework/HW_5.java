9f886d7f8ab5f94326775c370656b0bdf8aa5eb7
DamirLapeiko <lapeiko02@mail.ru>
2023-01-18 19:44:22 +0300

    Homework

--- src/Homework/HW_5.java
+++ src/Homework/HW_5.java
@@ -3,22 +3,13 @@ import java.util.Random;
 //Создайте двумерный массив. Выведите на консоль диагонали массива.
 public class HW_5 {
     public static void main(String[] args) {
-        int[][] array = new int[5][5];
-        Random random = new Random();
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                array[i][j] = random.nextInt(15);
-                if (i == j) {
-                    System.out.print(array[i][j] + " ");
-                } else if (i + j == array.length - 1) {
-                    System.out.print(array[i][j] + " ");
-                } else {
-                    array[i][j] = 0;
-                    System.out.print(array[i][j] + " ");
-                }
-            }
-            System.out.println();
+        int[][] array = {{3, 0, 1}, {5, 0, 10}, {0, 9, 0}};
+        for (int l = 0; l < array.length; l++){
+            System.out.print(array[l][l] + " ");
         }
         System.out.println();
+        for (int k = 0; k < array.length; k++){
+            System.out.print(array[k][array.length - k - 1] + " ");
+        }
     }
 }
